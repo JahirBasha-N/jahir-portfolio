ea7311c117a6200b78ea3d420edf9bdbb7e9aef0
JahirBasha <jahirbasha959@gmail.com>
2024-03-09 01:14:32 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <WelcomeToPortFolio msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import WelcomeToPortFolio from './components/WelcomeToPortFolio.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WelcomeToPortFolio
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>./components/WelcomeToPortFolio.vue
